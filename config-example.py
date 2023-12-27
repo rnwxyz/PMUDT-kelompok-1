513eaeed8b635082cc7860a305f1c99b032f5698
rnwxyz <jamesstrom78@gmail.com>
2023-12-27 08:52:36 +0800

    fix : config

--- config-example.py
+++ config-example.py
@@ -1,7 +1,7 @@
-DB_HOST = "localhost"
-DB_USER = "root"
-DB_PASS = "root"
-DB_NAME = "flask"
-DB_PORT = "3306"
-
-SECRET_KEY = "secretkey"
+class Config:
+    DB_HOST = "localhost"
+    DB_USER = "root"
+    DB_PASS = ""
+    DB_NAME = "flask"
+    DB_PORT = "3306"
+    SECRET_KEY = "secretkey"
